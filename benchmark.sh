4aeb4266c5edffa3f0949377e143be2f1eb335a1
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2026-08-11 20:49:41 +0100

    UPDATE: benchmark.sh now checks for hyperfine and installs if not installed

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cargo build --release && hyperfine --warmup 5 -n "Multiplicative Persistence" -r 50 'cargo run --release'
+if ! command -v hyperfine >/dev/null 2>&1
+then
+    sudo apt install hyperfine -y
+fi
+
+cargo build --release
+hyperfine --warmup 5 -n "Multiplicative Persistence" -r 50 'cargo run --release'
